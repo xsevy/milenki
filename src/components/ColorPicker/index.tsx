59a5aa76790d136667bc3ffe1353ad35395582ad
Seweryn Piórkowski <git@piorkowski.email>
2024-12-26 15:36:54 +0100

    add colors

--- src/components/ColorPicker/index.tsx
+++ src/components/ColorPicker/index.tsx
@@ -1,3 +1,19 @@
 export const ColorPicker = () => {
-  return <>Color picker</>;
+  const colors = ["red", "green", "yellow", "blue", "purple", "pink", "black"];
+  return (
+    <>
+      {colors.map((color) => (
+        <div
+          style={{
+            width: "40px",
+            height: "40px",
+            backgroundColor: color,
+            marginRight: "4px",
+            marginLeft: "4px",
+            borderRadius: "24px",
+          }}
+        ></div>
+      ))}
+    </>
+  );
 };
